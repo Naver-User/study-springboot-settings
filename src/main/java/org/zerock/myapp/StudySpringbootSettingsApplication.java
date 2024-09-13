ad3b84fcd7f725f51e67bcba6c9fde0421304317
Yoseph-User <cherryneo@yahoo.co.kr>
2024-09-13 17:19:02 +0900

    Initial Commit

---
+++ src/main/java/org/zerock/myapp/StudySpringbootSettingsApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootSettingsApplication.class, args);
+	}
+
+}
